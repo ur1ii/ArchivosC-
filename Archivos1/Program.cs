56d6daa31ffc8708717babcf5f19fdc79d57af17
HUMBERTO <hrruiz@uamv.edu.ni>
2026-06-01 10:47:35 -0600

    Agregue el destinatario.

--- Archivos1/Program.cs
+++ Archivos1/Program.cs
@@ -6,7 +6,8 @@ int menu()
     Console.WriteLine("=== MENU ===");
     Console.WriteLine("1. Agregar");
     Console.WriteLine("2. Mostrar");
-    Console.WriteLine("3. Salir");
+    Console.WriteLine("3. Guardar");
+    Console.WriteLine("4. Salir");
     Console.WriteLine("Ingresa tu opcion: ");
     return int.Parse(Console.ReadLine()!);
 
@@ -16,7 +17,7 @@ void pedirdatos()
 {
     for (int i = 0; i < 10; i++)
     {
-        Console.WriteLine("=== Ingresa las notas ===");
+        Console.WriteLine("=== Ingresa los datos ===");
         Console.WriteLine("Nombre: ");
         estudiante[i].nombre = Console.ReadLine()!;
         Console.WriteLine("Carrera: ");
@@ -34,6 +35,18 @@ void mostrardatos()
         Console.WriteLine($"{estudiante[i].nombre} | {estudiante[i].carrera} | {estudiante[i].promedio}");
     }
 }
+
+void guardarachivos()
+{
+    StreamWriter archivo = new StreamWriter("C:\\Archivos_R");
+    for (int i = 0; i < 10; i++)
+    {
+        archivo.WriteLine(estudiante[i].nombre + ";" + estudiante[i].carrera + ";" + estudiante[i].promedio);
+    }
+    archivo.Close();
+    Console.WriteLine("Registro guardado...");
+}
+   
 void main()
     {
        int op;
@@ -46,8 +59,12 @@ void main()
                     pedirdatos();
                     break;
                 case 2:
+                    mostrardatos();
                     break;
                 case 3:
+                    guardarachivos();
+                    break;
+                case 4:
                     Console.WriteLine("Adios...");
                     break;
                 default:
